5beb6586809aa02ce9f04496eb8bc0a5e47bc149
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-06-07 23:07:20 +0100

    working on adding videos to the app

--- app/index.jsx
+++ app/index.jsx
@@ -8,8 +8,8 @@ import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
   const { loading, isLogged } = useGlobalContext();
-
   if (!loading && isLogged) return <Redirect href="/home" />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
